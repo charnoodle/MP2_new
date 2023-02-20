f417f1b80176858be34a1bebb4248272a88da93e
charnoodle <chadavidcpa@gmail.com>
2023-02-20 12:42:27 +0800

    Fixed about us images

--- qrcode.js
+++ qrcode.js
@@ -1,4 +1,4 @@
 var qrcode = new QRCode(document.querySelector(".qrcode"));
-qrcode.makeCode("Why did you scanned me?")
+qrcode.makeCode("Why did you scan me?")
 function generateQr(){  qrcode.makeCode(document.querySelector("input").value)
 }
